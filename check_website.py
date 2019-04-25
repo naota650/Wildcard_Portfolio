09d6871efebecfcd415d8af95aca20f408d62253
root <root@DESKTOP-S28FFUL.localdomain>
2019-04-24 22:38:49 -0700

    added checks and intructions for how to use argv for project

--- check_website.py
+++ check_website.py
@@ -1,4 +1,5 @@
 from sys import argv
+import sys
 from urllib.request import urlopen
 from smtplib import SMTP
 import re
@@ -9,4 +10,23 @@ import threading
 
 
 html = urlopen("http://www.google.com/")
-print(html.read())
+#print(html.read())
+
+def main():
+    if len(sys.argv) > 1:
+        site_url = argv[1]
+    else:
+        print("first argv must be website url")
+        exit
+    
+    if len(sys.argv) > 2:
+        check_interval = int(argv[2])
+    else:
+        print("second argv must be time interval")
+        exit
+
+    print(html)
+    #print(html.read())
+
+if __name__ == '__main__':
+    main()
