6da83d0aede3eee5271e256f53feb2f0b6a966cf
root <root@DESKTOP-S28FFUL.localdomain>
2019-04-24 22:51:07 -0700

    added func to check that user is connected to internet

--- check_website.py
+++ check_website.py
@@ -1,5 +1,6 @@
 from sys import argv
 import sys
+import requests
 from urllib.request import urlopen
 from smtplib import SMTP
 import re
@@ -12,7 +13,9 @@ import threading
 html = urlopen("http://www.google.com/")
 #print(html.read())
 
-def main():
+# Checks that correct arguments are present and shows
+# correct usage if error occurs
+def check_argvs():
     if len(sys.argv) > 1:
         site_url = argv[1]
     else:
@@ -25,6 +28,22 @@ def main():
         print("second argv must be time interval")
         exit
 
+# Checks that user has connection, checks google and yahoo
+# because its not likely that both are down lol
+def check_connection():
+    g = requests.get('http://google.com')
+    y = requests.get('http://yahoo.com')
+    if g:
+        print ("Google is online")
+    if y:
+        print ("Yahoo is online")
+    if not g and not y:
+        print ("internet down")
+
+
+def main():
+    check_argvs()
+    check_connection()
     print(html)
     #print(html.read())
 
