f2f8dad9b522b8db1936930916245ca940c3f3b7
root <root@DESKTOP-S28FFUL.localdomain>
2019-04-24 23:51:25 -0700

    adde main functionality, is able to check if website is down or not, however need to find a way to check if the url itself is a valid oen or else it breaks

--- check_website.py
+++ check_website.py
@@ -7,7 +7,7 @@ import re
 import time
 import sched
 import threading
-
+from urllib.parse import urlparse
 
 
 html = urlopen("http://www.google.com/")
@@ -31,19 +31,51 @@ def check_argvs():
 # Checks that user has connection, checks google and yahoo
 # because its not likely that both are down lol
 def check_connection():
-    g = requests.get('http://google.com')
-    y = requests.get('http://yahoo.com')
+    g = confirm_status('http://google.com')
+    y = confirm_status('http://yahoo.com')
+
     if g and y:
-        print ("internet is connected")
+        #print ("internet is connected")
         return True
     if not g and not y:
         print ("internet down")
         return False
+# Opens user url and checks response status code to see if avail
+def confirm_status(url):
+    url = check_http(url)
+ #   check = is_url(url)
+ #   if is_url(url):
+ #       url_file = urlopen(url)
+ #   else:
+ #       print("url is not valid")
+ #       exit()
+    url_file = urlopen(url)
+    response = url_file.code
+    #print(response)
+    if response in (200, 302):
+        return True
+    else:
+        return False
+
+#def is_url(url):
+#    try:
+#        result = urlparse(url)
+#        if result:
+#            print ("is an urlparse")
+#        return all([result.scheme, result.netlock, result.path])
+#    except:
+#        return False
+
+# Adds http:// to url if doesn't already have it
+def check_http(url):
+    if not re.match('^http[s]?://', url):
+        url = "http://" + url
+    return url
 
 # Checks website connection of user's choice
 def check_url(url):
     if check_connection():
-        url_check = requests.get(url)
+        url_check = confirm_status(url)
         if url_check:
             print (url + " is online!!")
         else:
@@ -55,9 +87,8 @@ def main():
     check_argvs()
     #check_connection()
     site_url = argv[1]
-    print("url is " + site_url)
     check_url(site_url)
-    print(html)
+    #print(html)
     #print(html.read())
 
 if __name__ == '__main__':
